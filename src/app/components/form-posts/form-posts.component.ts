ec8ca5d02b9662c5a809b970e1a19838bbf037d5
miglehp <migle.hp@gmail.com>
2023-06-23 09:37:21 +0200

    corrección de estilos + invertir orden para que ultimos post se lean arriba

--- src/app/components/form-posts/form-posts.component.ts
+++ src/app/components/form-posts/form-posts.component.ts
@@ -1,27 +1,53 @@
 import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Post } from 'src/app/interfaces/post';
 import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-form-posts',
   templateUrl: './form-posts.component.html',
-  styleUrls: ['./form-posts.component.scss']
+  styleUrls: ['./form-posts.component.scss'],
 })
 export class FormPostsComponent {
-
   _postsService = inject(PostsService);
+  _router = inject(Router);
 
   formPost: FormGroup;
+  allPosts: Post[];
   postNuevo: Post | undefined;
 
-  constructor(){
+  constructor() {
+    this.allPosts = [];
+
     this.formPost = new FormGroup({
-      title: new FormControl(null, [Validators.required, Validators.minLength(5)]),
-      content: new FormControl(null, [Validators.required, Validators.minLength(30)]),
-      abstract: new FormControl(null, [Validators.required, Validators.minLength(10)]),
-      img: new FormControl(null, [Validators.required])
-    })
+      title: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+      content: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(30),
+      ]),
+      abstract: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(10),
+      ]),
+      img: new FormControl(null, [Validators.required]),
+    });
   }
 
+  ngOnInit() {
+    this.getAllPosts();
+  }
+
+  getAllPosts = () => (this.allPosts = this._postsService.getAll());
+
+  saveOnSubmit = () => {
+    this.postNuevo = this.formPost.value;
+    this.postNuevo!.id = this.allPosts.length + 1;
+    this.allPosts.push(this.postNuevo!);
+    this._postsService.saveAll(this.allPosts);
+    this._router.navigate(['posts']);
+  };
 }
