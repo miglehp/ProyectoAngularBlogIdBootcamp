d15bbb29bdc11dac9a49d241b6eeb467f2d5604f
miglehp <migle.hp@gmail.com>
2023-06-23 09:40:15 +0200

    funcionalidades para regresar cosas si no hay posts o no se consigue ese id

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -2,29 +2,28 @@ import { Injectable } from '@angular/core';
 import { Post } from '../interfaces/post';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PostsService {
-
-  constructor() { }
+  constructor() {}
 
   getAll = () => {
     const strPosts = localStorage.getItem('posts');
-    if(strPosts){
+    if (strPosts) {
       return JSON.parse(strPosts);
-    } 
-    return strPosts
-  }
+    }
+    return strPosts;
+  };
 
-  saveAll = (posts: Post[]) => localStorage.setItem('posts', JSON.stringify(posts));
+  saveAll = (posts: Post[]) =>
+    localStorage.setItem('posts', JSON.stringify(posts));
 
   getById = (id: number): Post | undefined => {
     const strPosts = localStorage.getItem('posts');
-  if(strPosts){
-    const arrPosts = JSON.parse(strPosts);
-    return arrPosts.find((post: Post) => post.id === id);
-  } 
-  return undefined;
-}
-
+    if (strPosts) {
+      const arrPosts = JSON.parse(strPosts);
+      return arrPosts.find((post: Post) => post.id === id);
+    }
+    return undefined;
+  };
 }
